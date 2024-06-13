a641a8a3e799c2d4f9544326452ddb16c38a09f8
humanimate <172449116+humanimate@users.noreply.github.com>
2024-06-13 16:14:50 +0530

    Added FollowYourPose

---
+++ methods/fyp/TextPoseBenchmark_t2vZero.py
@@ -0,0 +1,50 @@
+from benchmark_loader import TextPoseBench_loader
+from model import Model
+from omegaconf import OmegaConf
+from pathlib import Path
+
+import numpy as np
+import torch
+
+from PIL import Image
+
+
+class t2vZero_benchmark(object):
+    def __init__(self, config):
+        self.config = config
+        self.seed = config["seed"]
+        self.guidance_scale = config["guidance_scale"]
+        self.loader = TextPoseBench_loader(self.config)
+        self.device = torch.device("cuda:{}".format(config["gpu_id"]) if torch.cuda.is_available() else "cpu")
+        self.model = Model(device = self.device, dtype = torch.float16)
+        self.save_path = Path(config["save_path"])
+        self.save_dir = self.save_path/("".join(config["codebook_name"].split(".")[:-1]))
+        self.save_dir.mkdir(parents=True, exist_ok=True)
+
+    def draw_inference(self, prompt, pose_vid):
+        result = self.model.process_controlnet_pose(
+                                video_path=pose_vid,
+                                prompt=prompt, save_path="./",
+                                guidance_scale=self.guidance_scale,
+                                seed=self.seed,
+                        )
+        result = np.stack([(fr*255).astype(np.uint8) for fr in result]).astype(np.uint8)
+        return result
+
+    def run_benchmark(self):
+        for ndx in range(len(self.loader)):
+            bench_data = self.loader[ndx]
+            prompt = bench_data["prompt"]
+            pose_vid = torch.Tensor(bench_data["pose_seq"]).permute(0,3,1,2).to(self.device)
+            codebook_ndx = bench_data["codebook_index"]
+            res = self.draw_inference(prompt, pose_vid)
+            self.save_inferences(res, codebook_ndx)
+
+    def save_inferences(self, inference, codebook_ndx):
+        filename = "{:05d}.npy".format(codebook_ndx)
+        np.save(self.save_dir/filename, inference)
+
+if __name__ == "__main__":
+    cfg = OmegaConf.load("bench_runner.yaml")
+    obj = t2vZero_benchmark(cfg)
+    obj.run_benchmark()
---
+++ methods/fyp/benchmark_loader.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+from torch.utils.data import Dataset
+
+import math
+import numpy as np
+import pandas as pd
+
+
+def get_config(config_path):
+    """
+    Loads the config file and updates it with the command line arguments.
+    The model name is also updated. The config is then converted to a dictionary.
+    """
+    base_conf = OmegaConf.load(str(config_path))
+    overrides = OmegaConf.from_cli()
+    updated_conf = OmegaConf.merge(base_conf, overrides)
+    return OmegaConf.to_container(updated_conf)
+
+
+class TextPoseBench_loader(Dataset):
+    def __init__(self, config):
+        self.config = config
+        self.data_path = Path(self.config["data_path"])
+        self.vid_dir = self.data_path/self.config["pose_vid_dir"]
+        self.codebook = self.get_codebook(self.config["codebook_dir"], self.config["codebook_name"])
+        self.ndxs = self.get_dataloader_ndxs(self.config["num_chunks"], self.config["chunk_index"])
+        print("Loader ready | Start index: {} | End index: {} | Total rows: {}".format(self.ndxs[0], self.ndxs[-1], len(self.ndxs)))
+
+    def get_codebook(self, codebook_dir, codebook_name):
+        codebook = pd.read_csv(str(self.data_path/codebook_dir/codebook_name))
+        return codebook
+
+    def get_dataloader_ndxs(self, num_chunks, chunk_index):
+        assert num_chunks == -1 or num_chunks > chunk_index, "Chunk index cannot be greater or equal to the number of chunks"
+        assert chunk_index >= 0, "Chunk index cannot be negative"
+        ndxs = np.array(self.codebook.index.to_list())
+        total_rows = ndxs.shape[0]
+        if num_chunks != -1:
+            chunk_size = math.ceil(total_rows/num_chunks)
+            start = chunk_index*chunk_size
+            end = start + chunk_size
+            ndxs = ndxs[start:end]
+        return ndxs
+
+    def __getitem__(self, ndx):
+        codebook_ndx = self.ndxs[ndx]
+        row = self.codebook.iloc[codebook_ndx]
+        pose_seq_name = row["filename"]
+        prompt = row["prompt"]
+        pose_seq = np.load(self.data_path/self.vid_dir/pose_seq_name)
+        data = {
+            "prompt": prompt,
+            "pose_seq": pose_seq,
+            "codebook_index": codebook_ndx,
+        }
+        return data
+
+    def __len__(self):
+        return len(self.ndxs)
+
+
+if __name__ == "__main__":
+    cfg = get_config("configs/bench_runner.yaml")
+    loader = TextPoseBench_loader(cfg)
